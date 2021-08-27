d26493d731c6833385d1286f6e227f293c81fadc
Ronald Prajval <ronaldprajval7@gmail.com>
2021-08-27 15:15:26 +0530

    clean up

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,7 +3,6 @@ import classes from "./Counter.module.css";
 import { counterActions } from "../store/counter";
 
 const Counter = () => {
-  //dispatch is a function
   const dispatch = useDispatch();
 
   const counter = useSelector((state) => {
@@ -14,22 +13,18 @@ const Counter = () => {
   });
 
   const incrementHandler = () => {
-    //dispatch({ type: "increment" });
     dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    //dispatch({ type: "increase", amount: 10 });
     dispatch(counterActions.increase(10)); // {type: UNIQUE_ID. payload: 10}
   };
 
   const decrementHandler = () => {
-    //dispatch({ type: "decrement" });
     dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    //dispatch({ type: "toggle" });
     dispatch(counterActions.toggleCounter());
   };
 
